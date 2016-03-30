9b7a717929c4c373e68fe34d20b73c63aa451f46
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2016-03-30 02:02:55 +0200

    build task with browserify, babelify and uglify

--- gulp/build.js
+++ gulp/build.js
@@ -1,6 +1,18 @@
 'use strict'
 
 const
-  gulp = require('gulp')
+  gulp = require('gulp'),
+  utils = require('../utils'),
+  del = require('del'),
+  gulpconf = require('./gulpconf')
 
-gulp.task('build')
+
+gulp.task('browserify', gulpconf.bundler)
+
+gulp.task('build', ['clean', 'browserify'])
+
+gulp.task('clean', cleanup)
+
+function cleanup() {
+  return del([utils.paths.dist, utils.paths.tmp])
+}
---
+++ gulp/gulpconf.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const
+  gulp = require('gulp'),
+  path = require('path'),
+  utils = require('../utils'),
+
+  browserify = require('browserify'),
+  watchify = require('watchify'),
+
+  gsourcemaps = require('gulp-sourcemaps'),
+  guglify = require('gulp-uglify'),
+
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+
+  _ = require('underscore')
+
+let customOpts = {
+  entries: [path.join(utils.paths.client, '/', 'index.js')],
+  debug: true
+}
+
+let opts = _.extendOwn({}, watchify.args, customOpts)
+
+let __bundler = browserify(customOpts)
+let __bundlerWatcher = watchify(browserify(opts))
+
+// make this exports available inside the project
+exports.__bundler = __bundler
+exports.__bundlerWatcher = __bundlerWatcher
+
+exports.bundler = function () {
+  return __bundler.transform('babelify', {
+      presets: ['es2015']
+    })
+    .bundle()
+    .on('error', function (err) {
+      utils.errorHandler('bunbler')(err)
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(utils.paths.tmp))
+    .pipe(buffer())
+    .pipe(gsourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(guglify())
+    .pipe(gsourcemaps.write('./'))
+    .pipe(gulp.dest(utils.paths.dist))
+}
